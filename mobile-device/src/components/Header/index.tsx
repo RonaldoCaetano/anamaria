1ad0545b8b0478c0d91e0f1f5b059fb1640194f6
ronaldo.caetano <ronaldo.caetano@acct.global>
2020-06-13 15:43:36 -0300

    config files

---
+++ mobile-device/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
